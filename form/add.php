1ac2ffc227ad791399310883b2d58d5da98fe4f6
RashikTimalsina <rashiktimalsina21@gmail.com>
2025-02-13 21:48:07 +0545

    Add Students details

---
+++ form/add.php
@@ -0,0 +1,49 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', 'CLEB2d@mysql', 'bca');
+
+if (!$conn) {
+    die("Database connected failed: " . mysqli_connect_error());
+}
+
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+
+   $sql = "INSERT INTO students (name, email, address) VALUES ('$name', '$email', '$address')";
+     $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "Recorded";
+    } else {
+        echo "Not Recorded";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <blockquote>
+        <a href="student.php">Home</a> &ensp;
+        <a href="add.php">Add Student</a>
+        <hr>
+        <form action="" method="post">
+            <label for="name">Name</label> <br>
+            <input type="text" name="name" required> <br>
+            <label for="email">Email</label> <br>
+            <input type="text" name="email" required> <br>
+            <label for="address">Address</label> <br>
+            <input type="text" name="address" required> <br>
+            <button>Add Record</button>
+        </form>
+    </blockquote>
+</body>
+
+</html>
